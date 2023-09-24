5fd5214c397f53ad46b0494401e6060bb7bc7130
aoikurokawa <ht.so.ik0102@gmail.com>
2023-09-24 10:27:30 +0900

    feat: add logic of web scraping to main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,61 @@
-use std::env;
+use std::{
+    env,
+    fs::File,
+    io::{Read, Write},
+};
 
 use dotenv::dotenv;
+use scraper::{Html, Selector};
 use serde_json::{json, Map, Value};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     dotenv().expect("can not find env file");
 
+    // read env variables
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("failed to read TELEGRAM_BOT_TOKEN");
     let chat_id: i64 = env::var("TELEGRAM_CHAT_ID")
-        .expect("fail to read TELEGRAM_CHAT_ID")
+        .expect("failed to read TELEGRAM_CHAT_ID")
         .parse()
-        .expect("faile to parse");
+        .expect("failed to parse");
+    let url = env::var("TARGET_URL").expect("failed to read TARGET_URL");
 
-    println!("token: {}, chat_id: {}", token, chat_id);
+    let res = reqwest::blocking::get(&url).expect("Could not load url.");
 
-    // send_message("Test from library".to_string(), &token, chat_id).expect("fail to send message");
+    let raw_html_string = res.text().unwrap();
+
+    let document = Html::parse_document(&raw_html_string);
+
+    let app_data_selector = Selector::parse("#__APP_DATA").unwrap();
+
+    let app_data_elem = document.select(&app_data_selector).next().unwrap();
+    let app_data = app_data_elem.text().next().unwrap();
+
+    let parsed: Value = serde_json::from_str(app_data).unwrap();
+
+    let title = &parsed["appState"]["loader"]["dataByRouteId"]["2a3f"]["catalogs"][0]["articles"]
+        [0]["title"];
+
+    match File::open("latest_post_title.txt") {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+
+            if contents != title.to_string() {
+                let mut file = File::create("latest_post_title.txt").unwrap();
+                file.write_all(title.to_string().as_bytes())?;
+
+                // send message by Telegram
+                let msg = format!("New post {} has released. Check out {}", title, url);
+                send_message(msg, &token, chat_id).expect("fail to send message");
+            }
+        }
+        Err(_) => {
+            let mut file = File::create("latest_post_title.txt").unwrap();
+            file.write_all(title.to_string().as_bytes())?;
+        }
+    }
+
+    Ok(())
 }
 
 fn send_message(
